230d99bda92eba86bcbbf3b77efc791da650dfd5
Amanda Li <a383li@uwaterloo.ca>
2024-07-12 16:20:52 -0400

    got camera to start up

--- pantryAI/app/(tabs)/create/index.tsx
+++ pantryAI/app/(tabs)/create/index.tsx
@@ -1,12 +1,113 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image, Button } from 'react-native';
+import { useFonts } from 'expo-font';
+import { Raleway_600SemiBold, Raleway_700Bold } from '@expo-google-fonts/raleway';
+import { Nunito_400Regular, Nunito_500Medium, Nunito_600SemiBold, Nunito_700Bold } from '@expo-google-fonts/nunito';
+import { LinearGradient } from 'expo-linear-gradient';
+import { router } from 'expo-router';
+import { responsiveHeight, responsiveWidth } from "react-native-responsive-dimensions";
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "react-native-responsive-screen";
+import { CameraView, useCameraPermissions } from 'expo-camera';
+import { useState } from 'react';
+
 
 export default function Create() {
-  return (
-    <View>
-      <Text>index</Text>
-    </View>
-  )
-}
+    let [fontsLoaded, fontError] = useFonts({
+        Raleway_700Bold,
+        Nunito_400Regular,
+        Nunito_700Bold,
+        Nunito_500Medium,
+        Nunito_600SemiBold,
+        Raleway_600SemiBold,
+    });
 
-const styles = StyleSheet.create({})
+    if (!fontsLoaded && !fontError) {
+        return null;
+    }
+    const [facing, setFacing] = useState('back');
+    const [permission, requestPermission] = useCameraPermissions();
+  
+    if (!permission) {
+      // Camera permissions are still loading.
+      return <View />;
+    }
+  
+    if (!permission.granted) {
+      // Camera permissions are not granted yet.
+      return (
+        <View style={styles.container}>
+          <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+          <Button onPress={requestPermission} title="grant permission" />
+        </View>
+      );
+    }
+  
+    function toggleCameraFacing() {
+      setFacing(current => (current === 'back' ? 'front' : 'back'));
+    }
+
+    const submit = () => {
+
+    }
+
+    return (
+      <LinearGradient
+            colors={["#E5ECF9", "#F6F7F9", "#E8EEF9"]}
+            style={{ flex: 1, paddingHorizontal: 10 }}>
+              <View style={styles.container}>
+              <CameraView facing='back'>
+              
+          <Text style={styles.title}>Let's look into your pantry</Text>
+          <TouchableOpacity style={styles.content} onPress={toggleCameraFacing}>
+            <View style={styles.buttonContainer}>
+              <Text style={styles.upload}>Upload Here</Text>
+            </View>
+            <Text>Press to scan your fridge</Text>
+    
+          </TouchableOpacity>
+          </CameraView>
+        </View>
+
+      </LinearGradient>
+
+    )}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+    marginTop: 50,
+  },
+  upload: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color:"#99ABC1",
+    fontFamily: "Nunito_500Medium",
+  },
+  content: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: 'black',
+    padding: 30,
+    borderRadius: 10,
+    aspectRatio: 1,
+     backgroundColor: "#fff"
+  },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    
+  
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 20,
+    fontFamily: "Raleway_700Bold",
+    color: "#14293A"
+  },
+})
